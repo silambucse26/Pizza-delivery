1372863e5252417819942e6cd93ff27a6ab3099a
silambarasan <csesilambu3@gmail.com>
2025-04-22 17:01:25 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: "/Pizza-delivery/",
     plugins: [react()],
+    base: process.env.React_base_path || "/Pizza-delivery",
 
 });
